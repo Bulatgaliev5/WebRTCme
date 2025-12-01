dedf74538f0c870f26123f6209074e3cd246ca67
Admin <Admin@DESKTOP-V6V9F0T>
2025-12-01 20:53:54 +0500

    Добавьте файлы проекта.

---
+++ WebRTCme.DemoApp/WebRTCme.DemoApp.Maui/MauiProgram.cs
@@ -0,0 +1,66 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
+using WebRTCme.Middleware;
+using Xamarinme;
+namespace WebRTCme.DemoApp.Maui;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			})
+			.ConfigureMauiHandlers(handlers =>
+			{
+				handlers.AddHandler(typeof(Media), typeof(MediaHandler));
+			});
+
+		builder.Configuration.AddConfiguration(
+			new ConfigurationBuilder()
+			.AddEmbeddedResource(new EmbeddedResourceConfigurationOptions
+			{
+                Assembly = Assembly.GetExecutingAssembly(),
+                Prefix = "WebRTCme.DemoApp.Maui"
+            })
+			.Build());
+
+
+        var webRtcMiddleware = CrossWebRtcMiddlewareMaui.Current;
+
+        builder.Services.AddSingleton(webRtcMiddleware.WebRtc);
+        builder.Services.AddSingleton(webRtcMiddleware);
+
+        builder.Services.AddMauiMiddleware();
+
+#if DEBUG
+        // ТОЛЬКО для разработки - отключаем проверку SSL
+        System.Net.ServicePointManager.ServerCertificateValidationCallback +=
+            (sender, certificate, chain, sslPolicyErrors) => true;
+
+        // Или через HttpClientHandler
+        builder.Services.AddSingleton(sp =>
+        {
+            var handler = new HttpClientHandler();
+#if ANDROID
+            handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+            {
+                // Разрешаем для локального IP
+                if (cert != null && cert.Issuer.Contains("192.168.0.114"))
+                    return true;
+                return errors == System.Net.Security.SslPolicyErrors.None;
+            };
+#endif
+            return new HttpClient(handler);
+        });
+#endif
+
+        return builder.Build();
+	}
+}
